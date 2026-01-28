a273873ee75aed820afa590529f0de4a7b2ca7a7
xuantion107 <xuantionzang@gmail.com>
2026-01-28 09:53:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cron = require('node-cron');
 
 // Konfigurasi
-const BOT_TOKEN = process.env.BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const BOT_TOKEN = process.env.BOT_TOKEN || '8538468032:AAH_a-ZUrV7hH80h29i_rCXIeILAvzQdFYI';
 const HERO_SMS_API_KEY = process.env.HERO_SMS_API_KEY || 'YOUR_HERO_SMS_API_KEY';
 const ATLANTIC_API_KEY = process.env.ATLANTIC_API_KEY || 'YOUR_ATLANTIC_API_KEY';
 const ADMIN_ID = process.env.ADMIN_ID ? parseInt(process.env.ADMIN_ID) : 123456789; // Ganti dengan ID Telegram admin
